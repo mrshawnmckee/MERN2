a2fa7b98486663c1b826f97baec47ecf71deecb2
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2022-12-09 20:36:47 -0500

    Add files via upload

--- propitup/src/Components/PersonCard.js
+++ propitup/src/Components/PersonCard.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PersonCard = (props) => {
+    const [ currentAge, setCurrentAge ] = useState(props.age);
     return (
         <div>
             <h1>{props.fullName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {currentAge}</p>
             <p>Hair Color: {props.hairColor}</p>
+            
+            <button onClick={(event) => setCurrentAge(currentAge + 1 ) }>Birthday for {props.name}</button>
         </div>
     )
 }
